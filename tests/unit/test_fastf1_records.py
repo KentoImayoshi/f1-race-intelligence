9210f4979a4d8438ce388ac76ae15df76b559d35
Kento Imayoshi <petersonkentoimayoshipereira@gmail.com>
2026-03-12 23:58:02 -0300

    test: clarify fastf1 records type error

---
+++ tests/unit/test_fastf1_records.py
@@ -0,0 +1,9 @@
+import pytest
+
+from f1_ingestion.ingestion import _records_from_results
+
+
+@pytest.mark.unit
+def test_records_from_results_rejects_unknown_type() -> None:
+    with pytest.raises(TypeError, match="FastF1 results object must be a list of dicts or a pandas DataFrame"):
+        _records_from_results(object())
